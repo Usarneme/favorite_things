5aaf60062dae2aaf4fd9ba249fe82612f203852d
Usarneme <jamestcoop@gmail.com>
2021-03-29 10:47:39 -0700

    get values from each of the four inputs. store them in an array.
    Co-authored-by: Michael Burton <micbur1@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
+  // console.log("do we get here?")
+
   $("form").submit(function(event) {
+    // console.log("what about here?")
     event.preventDefault();
-    let array=[]
-    let thing1 =
+    let array = []
+    let thing1Val = $("#thing1").val();
+    array.push(thing1Val);
+
+    array.push($("#thing2").val());
+    array.push($("#thing3").val());
+    array.push($("#thing4").val());
+    console.log(array);
   });
 });
+
+
